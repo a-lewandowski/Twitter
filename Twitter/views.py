4b79ab44c8373c2d237ae0a6820be707e1a36fec
Adam Lewandowski <adam_lewandowski@yahoo.com>
2019-05-11 15:10:49 +0200

    setting up twitter

--- Twitter/views.py
+++ Twitter/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django import views
+
+
+class MainWebpageView(views.View):
+
+    def get(self, request):
+        ctx = {}
+        return render(request, 'twitter/index.html', ctx)
 
 # Create your views here.
